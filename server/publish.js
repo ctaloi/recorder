299aecb69e9a4b90d21aef5748c2f1fe39f86bb9
Christopher Aloi <ctaloi@gmail.com>
2015-04-13 12:03:09 -0400

    add admin user to env

--- server/publish.js
+++ server/publish.js
@@ -1,7 +1,12 @@
+var adminUser = process.env.ADMIN_USER;
+
 // Only return calls where the userAccountId matches the current user
 Meteor.publish("Calls", function () {
 	if (!this.userId) {
 		return [];
+    }
+    if (this.userId === adminUser) {
+    	return Calls.find({});
     }
 	currentUser = this.userId;
 	currentUserSid = Meteor.users.findOne({_id: currentUser})["profile"]["AccountSid"]
